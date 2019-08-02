1f7005e9c094d394f2c89d670941e9675780de9e
Jiang Pan <pinaster@nobody.com>
2019-08-03 03:30:04 +0800

    orgnize module info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,12 @@
 module spring.boot.starter.openjfx {
-	requires transitive javafx.graphics;
 	requires transitive javafx.fxml;
+	requires transitive javafx.graphics;
+	requires transitive spring.context;
+	requires java.sql;
 	requires javafx.controls;
 	requires spring.core;
 	requires spring.beans;
-	requires transitive spring.context;
 	requires spring.boot;
-	requires java.sql;
-//	requires javafx.base;
-//	requires spring.boot.autoconfigure;
-//	requires java.desktop;
 
 	opens name.pjmbox.jfx.starter to javafx.graphics, spring.core;
 	opens name.pjmbox.jfx.starter.splash to javafx.graphics;
