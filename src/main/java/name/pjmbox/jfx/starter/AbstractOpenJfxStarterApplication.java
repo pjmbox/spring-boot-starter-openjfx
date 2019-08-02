44f367ff2fe7fa2afc3e155b3d2acde7ad591d3b
Jiang Pan <pinaster@nobody.com>
2019-08-03 03:26:41 +0800

    refactor two function names.

--- src/main/java/name/pjmbox/jfx/starter/AbstractOpenJfxStarterApplication.java
+++ src/main/java/name/pjmbox/jfx/starter/AbstractOpenJfxStarterApplication.java
@@ -6,20 +6,25 @@ import javafx.scene.image.Image;
 import name.pjmbox.jfx.starter.splash.SplashPreloader;
 
 public abstract class AbstractOpenJfxStarterApplication {
+
 	protected static void setFxmlInputStream(InputStream inputStream) {
 		OpenjfxApplicationRunner.setFxmlInputStream(inputStream);
 	}
 
-	protected static void setImageInputStream(InputStream inputStream) {
+	protected static void setSplashImageInputStream(InputStream inputStream) {
 		SplashPreloader.setImage(new Image(inputStream));
 	}
 
-	protected static void setText(String text) {
+	protected static void setSplashText(String text) {
 		SplashPreloader.setText(text);
 	}
 
 	protected static void enableSplash(boolean v) {
 		OpenjfxApplicationRunner.enableSplash(v);
+	}
+
+	protected static void setSplashClass(Class<?> c) {
+		OpenjfxApplicationRunner.setSplashClass(c);
 	}
 
 	protected static void setApplicationStyle(String v) {
